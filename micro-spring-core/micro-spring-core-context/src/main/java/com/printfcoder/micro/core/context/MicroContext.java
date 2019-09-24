992bfd38cf3c0c4ab40ec76ae79067ac19a3e1db
shu xian <i@shux.me>
2019-09-24 12:54:52 +0800

    set context value

--- micro-spring-core/micro-spring-core-context/src/main/java/com/printfcoder/micro/core/context/MicroContext.java
+++ micro-spring-core/micro-spring-core-context/src/main/java/com/printfcoder/micro/core/context/MicroContext.java
@@ -1,5 +1,8 @@
 package com.printfcoder.micro.core.context;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * RPC context
  * <p>
@@ -16,12 +19,29 @@ public class MicroContext implements Cloneable {
         local.set(context);
     }
 
+    private Map<String, Object> values = new ConcurrentHashMap<String, Object>();
+
     public static MicroContext getContext() {
         MicroContext context = local.get();
         if (context == null) {
             context = new MicroContext();
             local.set(context);
         }
+
         return context;
     }
+
+    public MicroContext setValue(String key, Object value) {
+        if (key != null) {
+            if (value == null) {
+                values.remove(key);
+            } else {
+                values.put(key, value);
+            }
+        }
+
+        return this;
+    }
+
+
 }
