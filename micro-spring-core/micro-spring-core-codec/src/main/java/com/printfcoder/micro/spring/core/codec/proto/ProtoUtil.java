07dfe12500e26f8326da3d497080c54f115772de
Shu Xian <i@shux.me>
2019-10-22 00:00:57 +0800

    update

--- micro-spring-core/micro-spring-core-codec/src/main/java/com/printfcoder/micro/spring/core/codec/proto/ProtoUtil.java
+++ micro-spring-core/micro-spring-core-codec/src/main/java/com/printfcoder/micro/spring/core/codec/proto/ProtoUtil.java
@@ -1,280 +1,147 @@
-/**
- * Copyright 2016-2017 Sixt GmbH & Co. Autovermietung KG
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may obtain a
- * copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- * <p>
- * modified by printfcoder
- */
-
 package com.printfcoder.micro.spring.core.codec.proto;
 
-import com.google.gson.*;
-import com.google.protobuf.Message;
-import com.google.protobuf.util.JsonFormat;
+import com.google.protobuf.MessageLite;
 import com.printfcoder.micro.spring.core.common.exception.MicroRpcException;
-import com.printfcoder.micro.spring.core.common.util.FileUtil;
-import org.apache.commons.lang3.StringUtils;
+import java.lang.reflect.Method;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
-import java.lang.reflect.Constructor;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+/**
+ * @author <a href=mailto:i@shux.me>Printfcoder</a>
+ * 2019/10/21
+ **/
 
 public class ProtoUtil {
 
-    public static final int MAX_HEADER_CHUNK_SIZE = 1000;
-    public static final int MAX_BODY_CHUNK_SIZE = 10_000_000;
-
-    private static <TYPE extends Message> TYPE.Builder getBuilder(Class<TYPE> messageClass) throws NoSuchMethodException,
-            InstantiationException, IllegalAccessException, java.lang.reflect.InvocationTargetException {
-
-        Constructor<TYPE> constructor = messageClass.getDeclaredConstructor();
-        constructor.setAccessible(true);
-        TYPE instance = constructor.newInstance();
-
-        return instance.newBuilderForType();
-    }
 
     /**
-     * NOTE: this is only using the first element of the JsonArray
+     * Cache of parseFrom method
      */
-    public static <TYPE extends Message> TYPE jsonToProtobuf(JsonArray request, Class<TYPE> messageClass) throws NoSuchMethodException,
-            InstantiationException, IllegalAccessException, java.lang.reflect.InvocationTargetException {
-        if (request == null || request.size() < 1 || request.get(0).isJsonNull()) {
-            return null;
-        }
-        return jsonToProtobuf(request.get(0).toString(), messageClass);
-    }
+    ConcurrentMap<Class, Method> parseFromMethodMap = new ConcurrentHashMap<Class, Method>();
 
     /**
-     * Converts a JSON String to a protobuf message
-     * <p>
-     * Note: Ignores unknown fields
+     * Cache of toByteArray method
+     */
+    ConcurrentMap<Class, Method> toByteArrayMethodMap = new ConcurrentHashMap<Class, Method>();
+
+    /**
+     * 请求参数类型缓存 {service+method:class}
+     */
+    private ConcurrentMap<String, Class> requestClassCache = new ConcurrentHashMap<String, Class>();
+
+    /**
+     * 返回结果类型缓存 {service+method:class}
+     */
+    private ConcurrentMap<String, Class> responseClassCache = new ConcurrentHashMap<String, Class>();
+
+    /**
+     * 从缓存中获取请求值类
      *
-     * @param input        the input String to convert
-     * @param messageClass the protobuf message class to convert into
-     * @return the converted protobuf message (null in case of null input)
+     * @param service 接口名
+     * @param methodName 方法名
+     * @return 请求参数类
      */
-    public static <TYPE extends Message> TYPE jsonToProtobuf(String input, Class<TYPE> messageClass) throws NoSuchMethodException,
-            InstantiationException, IllegalAccessException, java.lang.reflect.InvocationTargetException {
-        if (input == null) {
-            return null;
-        }
+    public Class getReqClass(String service, String methodName) {
 
-        if (!isValidJSON(input)) {
-            return (TYPE) getBuilder(messageClass).getDefaultInstanceForType();
+        String key = buildMethodKey(service, methodName);
+        Class reqClass = requestClassCache.get(key);
+        if (reqClass == null) {
+            // 读取接口里的方法参数和返回值
+            String interfaceClass = ConfigUniqueNameGenerator.getInterfaceName(service);
+            Class clazz = ClassUtils.forName(interfaceClass, true);
+            loadProtoClassToCache(key, clazz, methodName);
         }
-
-        try {
-            TYPE.Builder builder = getBuilder(messageClass);
-            JsonElement element = new JsonParser().parse(input);
-            cleanJsonElement(element);
-            JsonFormat.parser().ignoringUnknownFields().merge(element.toString(), builder);
-            return (TYPE) builder.build();
-        } catch (Exception e) {
-            throw new RuntimeException("Error deserializing json to protobuf. Input = " + input, e);
-        }
+        return requestClassCache.get(key);
     }
 
-    private static void cleanJsonElement(JsonElement element) {
-        if (element.isJsonNull() || element.isJsonPrimitive()) {
-            return;
+    /**
+     * 从缓存中获取返回值类
+     *
+     * @param service 接口名
+     * @param methodName 方法名
+     * @return 请求参数类
+     */
+    public Class getResClass(String service, String methodName) {
+        String key = service + "#" + methodName;
+        Class reqClass = responseClassCache.get(key);
+        if (reqClass == null) {
+            // 读取接口里的方法参数和返回值
+            String interfaceClass = ConfigUniqueNameGenerator.getInterfaceName(service);
+            Class clazz = ClassUtils.forName(interfaceClass, true);
+            loadProtoClassToCache(key, clazz, methodName);
         }
-        if (element.isJsonArray()) {
-            cleanJsonArray(element.getAsJsonArray());
-        }
-        if (element.isJsonObject()) {
-            cleanJsonObject(element.getAsJsonObject());
-        }
+        return responseClassCache.get(key);
     }
 
-    private static void cleanJsonArray(JsonArray array) {
-        Iterator<JsonElement> iter = array.iterator();
-        while (iter.hasNext()) {
-            JsonElement ele = iter.next();
-            if (ele.isJsonNull()) {
-                iter.remove();
-                continue;
-            } else {
-                cleanJsonElement(ele);
+    /**
+     * 拼装缓存的key
+     *
+     * @param serviceName 接口名
+     * @param methodName 方法名
+     * @return Key
+     */
+    private String buildMethodKey(String serviceName, String methodName) {
+        return serviceName + "#" + methodName;
+    }
+
+    /**
+     * 加载protobuf接口里方法的参数和返回值类型到缓存，不需要传递
+     *
+     * @param key 缓存的key
+     * @param clazz 接口名
+     * @param methodName 方法名
+     */
+    private void loadProtoClassToCache(String key, Class clazz, String methodName) {
+        Method pbMethod = null;
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            if (methodName.equals(method.getName())) {
+                pbMethod = method;
+                break;
             }
         }
-    }
-
-    private static void cleanJsonObject(JsonObject element) {
-        Set<Map.Entry<String, JsonElement>> members = element.entrySet();
-        Iterator<Map.Entry<String, JsonElement>> iter = members.iterator();
-        while (iter.hasNext()) {
-            Map.Entry<String, JsonElement> member = iter.next();
-            JsonElement value = member.getValue();
-            cleanJsonElement(value);
+        if (pbMethod == null) {
+            throw new MicroRpcException(500, "Cannot found protobuf method: " + clazz.getName() + "." + methodName);
         }
+        Class[] parameterTypes = pbMethod.getParameterTypes();
+        if (parameterTypes == null
+            || parameterTypes.length != 1
+            || isProtoBufMessageObject(parameterTypes[0])) {
+            throw new MicroRpcException(500, "class based protobuf: " + clazz.getName() + ", only support one protobuf parameter!");
+        }
+        Class reqClass = parameterTypes[0];
+        requestClassCache.put(key, reqClass);
+        Class resClass = pbMethod.getReturnType();
+        if (resClass == void.class || !isProtoBufMessageClass(resClass)) {
+            throw new MicroRpcException(500, "class based protobuf: " + clazz.getName() + ", only support return protobuf message!");
+        }
+        responseClassCache.put(key, resClass);
     }
 
-    private static boolean isValidJSON(String input) {
-        if (StringUtils.isBlank(input)) {
+    /**
+     * Is this object instanceof MessageLite
+     *
+     * @param object unknown object
+     * @return instanceof MessageLite
+     */
+    boolean isProtoBufMessageObject(Object object) {
+        if (object == null) {
             return false;
         }
-
-        if (!input.startsWith("{")) {
-            return false;
-        }
-
-        try {
-            new JsonParser().parse(input);
-        } catch (JsonParseException ex) {
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Converts a byte array to a protobuf message
-     *
-     * @param data         the byte array to convert
-     * @param messageClass the protobuf message class to convert into
-     * @return the converted protobuf message
-     * @throws MicroRpcException if something goes wrong during the deserialization
-     */
-    public static <TYPE extends Message> TYPE byteArrayToProtobuf(byte data[], Class<TYPE> messageClass)
-            throws MicroRpcException {
-        try {
-            Message.Builder builder = getBuilder(messageClass);
-            return (TYPE) builder.mergeFrom(data).build();
-        } catch (Exception e) {
-            throw new MicroRpcException(500, "Error deserializing byte array to protobuf: " + e);
+        if (MULTIPLE_CLASSLOADER) {
+            return object instanceof MessageLite || isProtoBufMessageClass(object.getClass());
+        } else {
+            return object instanceof MessageLite;
         }
     }
 
     /**
-     * Creates an empty protobuf message of the specified type
+     * Is this class is assignable from MessageLite
      *
-     * @param klass the protobuf message type
-     * @return the generated protobuf message
+     * @param clazz unknown class
+     * @return is assignable from MessageLite
      */
-    public static <TYPE extends Message> TYPE newEmptyMessage(Class<TYPE> klass) {
-        try {
-            Message.Builder builder = getBuilder(klass);
-            return (TYPE) builder.build();
-        } catch (Exception e) {
-            throw new RuntimeException("Error deserializing byte array to protobuf", e);
-        }
-    }
-
-    /**
-     * Converts a protobuf message to a JSON object
-     * <p>
-     * Note: Preserves the field names as defined in the proto definition
-     *
-     * @param input the protobuf message to convert
-     * @return the converted JSON object
-     */
-    public static JsonObject protobufToJson(Message input) {
-        JsonObject object = new JsonObject();
-        if (input != null) {
-            try {
-                String jsonString = JsonFormat.printer()
-                        .preservingProtoFieldNames()
-                        .print(input);
-                object = new JsonParser().parse(jsonString).getAsJsonObject();
-            } catch (Exception e) {
-                throw new RuntimeException("Error deserializing protobuf to json", e);
-            }
-        }
-        return object;
-    }
-
-    /**
-     * Converts a protobuf message to a JSON object
-     * <p>
-     * Note: Camel-cases the field names as defined in the proto definition
-     *
-     * @param input the protobuf message to convert
-     * @return the converted JSON object
-     */
-    public static JsonObject protobufToJsonCamelCase(Message input) {
-        JsonObject object = new JsonObject();
-        if (input != null) {
-            try {
-                String jsonString = JsonFormat.printer()
-                        .print(input);
-                object = new JsonParser().parse(jsonString).getAsJsonObject();
-            } catch (Exception e) {
-                throw new RuntimeException("Error deserializing protobuf to json", e);
-            }
-        }
-        return object;
-    }
-
-    /**
-     * Converts a protobuf message to a JSON object
-     * <p>
-     * Note: Preserves the field names as defined in the proto definition
-     * Note:
-     *
-     * @param input the protobuf message to convert
-     * @return the converted JSON object
-     */
-    public static JsonObject protobufToJsonWithDefaultValues(Message input) {
-        JsonObject object = new JsonObject();
-        if (input != null) {
-            try {
-                String jsonString = JsonFormat.printer()
-                        .preservingProtoFieldNames()
-                        .includingDefaultValueFields()
-                        .print(input);
-                object = new JsonParser().parse(jsonString).getAsJsonObject();
-            } catch (Exception e) {
-                throw new RuntimeException("Error deserializing protobuf to json", e);
-            }
-        }
-        return object;
-    }
-
-    /**
-     * Converts a JSON object to a protobuf message.
-     * <p>
-     * Note: Ignores unknown fields
-     *
-     * @param builder the proto message type builder
-     * @param input   the JSON object to convert
-     * @return the converted protobuf message
-     */
-    public static Message fromJson(Message.Builder builder, JsonObject input) throws Exception {
-        JsonFormat.parser().ignoringUnknownFields().merge(input.toString(), builder);
-        return builder.build();
-    }
-
-    /**
-     * Converts a proto file name into a class name according to the rules defined by protobuf:
-     * https://developers.google.com/protocol-buffers/docs/reference/java-generated
-     * <p>
-     * The file name will be camel cased (and underscores, hyphens etc. stripped out).
-     *
-     * @param protoFileName The file name to process: e.g. my_service.proto
-     * @return The class name: MyService
-     */
-    public static String toClassName(String protoFileName) {
-
-        if (protoFileName == null) {
-            return null;
-        }
-        String fileName = FileUtil.stripPath(protoFileName);
-        fileName = FileUtil.stripExtension(fileName);
-
-        String parts[] = fileName.split("[^A-Za-z0-9]");
-
-        StringBuilder classNameBuilder = new StringBuilder();
-        for (String part : parts) {
-            classNameBuilder.append(StringUtils.capitalize(part));
-        }
-        return classNameBuilder.toString();
+    boolean isProtoBufMessageClass(Class clazz) {
+        return clazz != null && ClassUtils.isAssignableFrom(MessageLite.class, clazz);
     }
 }
---
+++ micro-spring-core/micro-spring-core-common/src/main/java/com/printfcoder/micro/spring/core/common/util/CommonUtils.java
@@ -0,0 +1,141 @@
+package com.printfcoder.micro.spring.core.common.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * copied from sofastack/sofa-rpc/CommonUtils
+ *
+ * @author <a href=mailto:i@shux.me>Printfcoder</a>
+ */
+public class CommonUtils {
+
+    public static <K, V> V putToConcurrentMap(ConcurrentMap<K, V> map, K key, V value) {
+        V old = map.putIfAbsent(key, value);
+        return old != null ? old : value;
+    }
+
+    public static boolean isTrue(String b) {
+        return b != null && StringUtils.TRUE.equalsIgnoreCase(b);
+    }
+
+    public static boolean isTrue(Boolean b) {
+        return b != null && b;
+    }
+
+    public static boolean isFalse(Boolean b) {
+        return b != null && !b;
+    }
+
+    public static boolean isFalse(String b) {
+        return b != null && StringUtils.FALSE.equalsIgnoreCase(b);
+    }
+
+    public static boolean isEmpty(Collection collection) {
+        return collection == null || collection.isEmpty();
+    }
+
+    public static boolean isNotEmpty(Collection collection) {
+        return collection != null && !collection.isEmpty();
+    }
+
+    public static boolean isEmpty(Map map) {
+        return map == null || map.isEmpty();
+    }
+
+    public static boolean isNotEmpty(Map map) {
+        return map != null && !map.isEmpty();
+    }
+
+    public static boolean isEmpty(Object[] array) {
+        return array == null || array.length == 0;
+    }
+
+    public static boolean isNotEmpty(Object[] array) {
+        return array != null && array.length > 0;
+    }
+
+    public static <T extends Number> T parseNum(T num, T defaultInt) {
+        return num == null ? defaultInt : num;
+    }
+
+    public static int parseInt(String num, int defaultInt) {
+        if (num == null) {
+            return defaultInt;
+        } else {
+            try {
+                return Integer.parseInt(num);
+            } catch (Exception e) {
+                return defaultInt;
+            }
+        }
+    }
+
+    public static long parseLong(String num, long defaultLong) {
+        if (num == null) {
+            return defaultLong;
+        } else {
+            try {
+                return Long.parseLong(num);
+            } catch (Exception e) {
+                return defaultLong;
+            }
+        }
+    }
+
+    public static boolean parseBoolean(String bool, boolean defaultInt) {
+        if (bool == null) {
+            return defaultInt;
+        } else {
+            return Boolean.parseBoolean(bool);
+        }
+    }
+
+    public static int[] parseInts(String nums, String sperator) {
+        String[] ss = StringUtils.split(nums, sperator);
+        int[] ints = new int[ss.length];
+        for (int i = 0; i < ss.length; i++) {
+            ints[i] = Integer.parseInt(ss[i]);
+        }
+        return ints;
+    }
+
+    public static <T> boolean listEquals(List<T> left, List<T> right) {
+        if (left == null) {
+            return right == null;
+        } else {
+            if (right == null) {
+                return false;
+            }
+            if (left.size() != right.size()) {
+                return false;
+            }
+
+            List<T> ltmp = new ArrayList<T>(left);
+            List<T> rtmp = new ArrayList<T>(right);
+            for (T t : ltmp) {
+                rtmp.remove(t);
+            }
+            return rtmp.isEmpty();
+        }
+    }
+
+    public static String join(Collection collection, String separator) {
+        if (isEmpty(collection)) {
+            return StringUtils.EMPTY;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Object object : collection) {
+            if (object != null) {
+                String string = StringUtils.toString(object);
+                if (string != null) {
+                    sb.append(string).append(separator);
+                }
+            }
+        }
+        return sb.length() > 0 ? sb.substring(0, sb.length() - separator.length()) : StringUtils.EMPTY;
+    }
+}
